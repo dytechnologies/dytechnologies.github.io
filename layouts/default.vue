7c8f4efe3cd079c22ad0332fb955e0f119461c31
Romadhan Prasetyo <romadhanedy@gmail.com>
2020-10-20 14:16:05 +0700

    :tada: Initial Commit

---
+++ layouts/default.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="fixed d-flex content-center items-center">
+    <Nuxt />
+  </div>
+</template>
+
+<script>
+    export default {
+        head: {
+            bodyAttrs: {
+                class: "main-page"
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    .fixed {
+        width: 100%;
+        height: 100%;
+        position: fixed;
+    }
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <div class="bg-landing"></div>
+        <div class="header">
+            Dytechnologies.co.id
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style scoped>
+</style>
